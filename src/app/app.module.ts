a791ac93110540544210fd63e92d07325895efc7
Sasha Laurent <sashalaurent78@gmail.com>
2022-10-29 16:27:33 +0200

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { DocumentListComponent } from './documents-list/document-list.component';
+import { DocumentFormComponent } from './document-form/document-form.component';
+
+const appRoutes: Routes = [
+  { path: 'documents', component: DocumentListComponent },
+  { path: 'documents/new', component: DocumentFormComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentListComponent,
+    DocumentFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
